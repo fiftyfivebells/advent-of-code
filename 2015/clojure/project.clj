275d8d544c1e0863e56d7172959fe5418876f635
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2024-06-24 21:50:03 -0400

    Renamed clojure 2015 advent of code directory to clojure

--- 2015/clojure/project.clj
+++ 2015/clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject advent-of-code "0.1.0-SNAPSHOT"
+(defproject clojure-solutions "0.1.0-SNAPSHOT"
   :description "Advent of Code 2015: clojure solutions"
   :url "https://github.com/fiftyfivebells/advent-of-code/tree/main/2015/clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
