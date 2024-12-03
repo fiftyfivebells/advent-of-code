82c12384f0d24c195dc4bfe59724492848dd1e8e
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2024-12-03 10:21:06 -0500

    Polish up and finish day 1 in clojure

--- 2024/clojure/advent-of-code/src/advent_of_code/day01.clj
+++ 2024/clojure/advent-of-code/src/advent_of_code/day01.clj
@@ -1,4 +1,54 @@
 (ns advent-of-code.day01
   (:require [clojure_utils.utils :as u]))
 
+;; Takes the input (a list of strings separated by spaces within each line with a newline between lines)
+;; and turns it into a list of lists of longs. The transpose at the end turns this from a long list of
+;; pairs of integers to a length two list of integers.
+(defn transform-input
+  [input]
+  (->> input
+       u/read-file
+       u/to-lines
+       (mapv u/strings->longs)
+       u/transpose-matrix))
+
+(defn pair-difference
+  [[a b]]
+  (abs (- a b)))
+
+;; Used in the reduce function, this just gets the difference between the two numbers in a pair
+;; and then adds them to the accumulator.
+(defn reducer
+  [acc pair]
+  (+ acc (pair-difference pair)))
+
+;; This function takes a list of pairs of integers and returns the sum of the absolute differences
+(defn total-distance
+  [pairs]
+  (let [left (sort (first pairs))
+        right (sort (second pairs))
+        zipped (map vector left right)]
+    (reduce reducer 0 zipped)))
+
+
+(defn day01-part-1
+  [input]
+  (->> input
+       transform-input
+       total-distance))
+
+(defn similarity-score
+  [freq val]
+  (* val (or (freq val) 0)))
+
+(defn day01-part-2
+  [input]
+  (let [pairs (transform-input input)
+        freqs (frequencies (second pairs))]
+    (->> (first pairs)
+         (map #(similarity-score freqs %))
+         (reduce +))))
+
+(day01-part-1 "day01.txt")
+(day01-part-2 "day01.txt")
 
