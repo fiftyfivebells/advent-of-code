a1f5443f6e5f6f4fb72090ef77cd065b79d3a384
Stephen Bell <stephenbell@Ellens-MacBook-Air.local>
2024-12-02 22:07:51 -0500

    Create the 2024 clojure directory

---
+++ 2024/clojure/advent-of-code/project.clj
@@ -0,0 +1,8 @@
+(defproject clojure-solutions "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2024: clojure solutions"
+  :url "https://github.com/fiftyfivebells/advent-of-code/tree/main/2024/clojure"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[clojure_utils "0.1.0-SNAPSHOT"]
+                 [org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns advent-of-code.code})
---
+++ 2024/clojure/advent-of-code/src/advent_of_code/day01.clj
@@ -0,0 +1,4 @@
+(ns advent-of-code.day01
+  (:require [clojure_utils.utils :as u]))
+
+
